a3b906ee64f40d92f776396354743197d5091593
skuhlmann <samkuhlmann@gmail.com>
2020-04-06 16:59:03 -0600

    gets new leaders query in place

--- packages/react-app/src/utils/Queries.js
+++ packages/react-app/src/utils/Queries.js
@@ -10,6 +10,82 @@ export const GET_BADGES = gql`
       proposalSubmissionCount
       rageQuitCount
       jailedCount
+      dissents
+      assents
+      memberships
+    }
+  }
+`;
+
+export const GET_LEADERS = gql`
+  query {
+    voteLeaders: badges(first: 5, orderBy: voteCount, orderDirection: desc) {
+      memberAddress
+      voteCount
+    }
+
+    summonsLeaders: badges(
+      first: 5
+      orderBy: summonCount
+      orderDirection: desc
+    ) {
+      memberAddress
+      summonCount
+    }
+
+    sponsorLeaders: badges(
+      first: 5
+      orderBy: proposalSponsorCount
+      orderDirection: desc
+    ) {
+      memberAddress
+      proposalSponsorCount
+    }
+
+    submissionLeaders: badges(
+      first: 5
+      orderBy: proposalSubmissionCount
+      orderDirection: desc
+    ) {
+      memberAddress
+      proposalSubmissionCount
+    }
+
+    rageLeaders: badges(
+      first: 5
+      orderBy: rageQuitCount
+      orderDirection: desc
+    ) {
+      memberAddress
+      rageQuitCount
+    }
+
+    jailedLeaders: badges(
+      first: 5
+      orderBy: jailedCount
+      orderDirection: desc
+    ) {
+      memberAddress
+      jailedCount
+    }
+
+    membershipsLeaders: badges(
+      first: 5
+      orderBy: memberships
+      orderDirection: desc
+    ) {
+      memberAddress
+      memberships
+    }
+
+    dissentsLeaders: badges(first: 5, orderBy: dissents, orderDirection: desc) {
+      memberAddress
+      dissents
+    }
+
+    assentsLeaders: badges(first: 5, orderBy: assents, orderDirection: desc) {
+      memberAddress
+      assents
     }
   }
 `;
--- packages/react-app/src/views/leaders/Leaders.js
+++ packages/react-app/src/views/leaders/Leaders.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_LEADERS } from "../../utils/Queries";
 
 const Leaders = () => {
+  const { loading, error, data } = useQuery(GET_LEADERS);
+
+  console.log("data", data);
+
   return (
     <div>
       <h1>Leaders</h1>
