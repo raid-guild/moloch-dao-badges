f8af60942ce8b14154b908044b5cb19424d2c608
skuhlmann <samkuhlmann@gmail.com>
2020-04-09 08:33:01 -0600

    fixes bad import

---
+++ packages/react-app/src/contracts/addresses.js
@@ -0,0 +1,8 @@
+const addresses = {
+  badgeNFT: {
+    kovan: "0xCB7d0E68D86AC5D05083e835C697BbB71236F8a1",
+    mainnet: "0xAA65E7c8BBf3F2C6d2d8634Fc830F050a55BBbF9"
+  }
+};
+
+export default addresses;
