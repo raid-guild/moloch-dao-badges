75250ac42f9b08d02887fee242fe5ccb0ed6fd45
Dekan <dekanbro@gmail.com>
2020-04-24 13:55:56 -0600

    get profiles for cert page and make a small profile component

---
+++ packages/react-app/src/components/account/SmallProfile.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Box from "3box";
+import makeBlockie from "ethereum-blockies-base64";
+import { Text } from "rebass";
+
+import { truncateAddr } from "../../utils/Helpers";
+
+const SmallProfile = ({ memberAddress }) => {
+  const [profile, setProfile] = useState();
+
+  useEffect(() => {
+    let isCancelled = false;
+
+    const set3BoxData = async () => {
+      try {
+        const profile = await Box.getProfile(memberAddress);
+
+        if (!isCancelled) {
+          setProfile(profile);
+        }
+      } catch (err) {
+        console.log("3box err", err);
+      }
+    };
+
+    set3BoxData();
+
+    return () => {
+      isCancelled = true;
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Text mr={5}>
+      {profile && profile.image ? (
+        <img
+          alt=""
+          width="40"
+          height="40"
+          style={{ backgroundColor: "#b5b5b5" }}
+          src={
+            profile.image
+              ? `https://ipfs.infura.io/ipfs/${profile.image[0].contentUrl["/"]}`
+              : null
+          }
+        />
+      ) : (
+        <img
+          alt=""
+          width="40"
+          height="40"
+          style={{ backgroundColor: "#b5b5b5" }}
+          src={makeBlockie(memberAddress)}
+        />
+      )}
+
+      {truncateAddr(memberAddress)}
+    </Text>
+  );
+};
+
+export default SmallProfile;
